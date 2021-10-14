1394c3ba4b83e8256983bb977b7408e2094e9881
Sodiq Alabi <adisco4420@gmail.com>
2021-10-14 14:24:40 +0100

    lazy load server module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/server/server.module').then(m => m.ServerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
